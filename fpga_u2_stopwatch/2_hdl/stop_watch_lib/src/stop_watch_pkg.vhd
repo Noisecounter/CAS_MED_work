1d0e079ec2766f389d13e947227e97740ac25527
Michael <hb9gfa@gmx.ch>
2025-09-29 18:20:57 +0200

    stop_watch_package.vhd neue Konstanten u. Array definiert. Kommentare hinzugefügt.

--- fpga_u2_stopwatch/2_hdl/stop_watch_lib/src/stop_watch_pkg.vhd
+++ fpga_u2_stopwatch/2_hdl/stop_watch_lib/src/stop_watch_pkg.vhd
@@ -2,15 +2,16 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
 PACKAGE constants IS
-CONSTANT c_max1_syn: integer := 124999;
-CONSTANT c_max2_syn: integer := 9;
+CONSTANT c_max1_syn:        integer := 124999;
+CONSTANT c_max2_syn:        integer := 9;
 
-CONSTANT c_max1_sim: integer := 4;
-CONSTANT c_max2_sim: integer := 4;
+CONSTANT c_max1_sim:        integer := 4;
+CONSTANT c_max2_sim:        integer := 4;
 
-END constants;
+CONSTANT c_num_digits:      natural range 0 to 7    := 4;                   -- Anzahl geünschter Digits
+TYPE digit_array_t is array (0 to c_num_digits) of natural range 0 to 9;    -- Definition des Array-Type mit Stellen 0 bis c_num_digits
 
-
-PACKAGE BODY constants IS
+CONSTANT c_max_value_syn:   digit_array_t := (5,9,9,9);                     -- Wertebereich der einzelnen Digits f. Synthese
+CONSTANT c_max_value_sim:   digit_array_t := (2,2,2,2);                     -- Wertebereich der einzelnen Digits f. Simulation
 
 END constants;
