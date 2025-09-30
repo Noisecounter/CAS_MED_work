d8fe5b205559f28794454ad02f914438f7467305
rast <michael.rast@psi.ch>
2025-09-30 09:03:27 +0200

    spop_watch_pkg.vhd kommentiert.

--- fpga_u2_stopwatch/2_hdl/stop_watch_lib/src/stop_watch_pkg.vhd
+++ fpga_u2_stopwatch/2_hdl/stop_watch_lib/src/stop_watch_pkg.vhd
@@ -2,11 +2,11 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
 PACKAGE constants IS
-CONSTANT c_max1_syn:        integer := 124999;
-CONSTANT c_max2_syn:        integer := 9;
+CONSTANT c_max1_syn:        integer := 124999;                              -- Teilt 125 MHz in 1 kHz (Prescaler)
+CONSTANT c_max2_syn:        integer := 9;                                   -- Teilt 1 kHz in 100 Hz (Prescaler)
 
-CONSTANT c_max1_sim:        integer := 4;
-CONSTANT c_max2_sim:        integer := 4;
+CONSTANT c_max1_sim:        integer := 4;                                   -- Teiler für Simulation (Prescaler)
+CONSTANT c_max2_sim:        integer := 4;                                   -- Teiler für Simulation (Prescaler)
 
 CONSTANT c_num_digits:      natural range 0 to 7    := 4;                   -- Anzahl geünschter Digits
 TYPE digit_array_t is array (0 to c_num_digits) of natural range 0 to 9;    -- Definition des Array-Type mit Stellen 0 bis c_num_digits
