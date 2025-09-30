0fcc873bf163b1733aa59f67309d0c4351b8b39e
rast <michael.rast@psi.ch>
2025-09-30 15:15:10 +0200

    c_max1/2_syn/sim von integer auf natural gewechselt.

--- fpga_u2_stopwatch/2_hdl/stop_watch_lib/src/stop_watch_pkg.vhd
+++ fpga_u2_stopwatch/2_hdl/stop_watch_lib/src/stop_watch_pkg.vhd
@@ -2,11 +2,11 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
 PACKAGE stop_watch_pkg IS
-CONSTANT c_max1_syn:        integer := 124999;                              -- Teilt 125 MHz in 1 kHz (Prescaler)
-CONSTANT c_max2_syn:        integer := 9;                                   -- Teilt 1 kHz in 100 Hz (Prescaler)
+CONSTANT c_max1_syn:        natural := 124999;                              -- Teilt 125 MHz in 1 kHz (Prescaler)
+CONSTANT c_max2_syn:        natural := 9;                                   -- Teilt 1 kHz in 100 Hz (Prescaler)
 
-CONSTANT c_max1_sim:        integer := 4;                                   -- Teiler für Simulation (Prescaler)
-CONSTANT c_max2_sim:        integer := 4;                                   -- Teiler für Simulation (Prescaler)
+CONSTANT c_max1_sim:        natural := 4;                                   -- Teiler für Simulation (Prescaler)
+CONSTANT c_max2_sim:        natural := 4;                                   -- Teiler für Simulation (Prescaler)
 
 CONSTANT c_num_digits:      natural range 0 to 7    := 4;                   -- Anzahl geünschter Digits
 TYPE digit_array_t is array (0 to c_num_digits) of natural range 0 to 9;    -- Definition des Array-Type mit Stellen 0 bis c_num_digits
