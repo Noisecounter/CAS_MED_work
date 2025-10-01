84e1dfef23bbe13947f265a2a2595298747159b9
rast <michael.rast@psi.ch>
2025-10-01 13:27:29 +0200

    stop_wtach_pkg.vhd zweite function f_select2 und entsprechender pkg body eingefügt.

--- fpga_u2_stopwatch/2_hdl/stop_watch_lib/src/stop_watch_pkg.vhd
+++ fpga_u2_stopwatch/2_hdl/stop_watch_lib/src/stop_watch_pkg.vhd
@@ -15,18 +15,28 @@ PACKAGE stop_watch_pkg IS
     CONSTANT c_max_value_syn:   digit_array_t := (5,9,9,9);                     -- Wertebereich der einzelnen Digits f. Synthese
     CONSTANT c_max_value_sim:   digit_array_t := (2,2,2,2);                     -- Wertebereich der einzelnen Digits f. Simulation
     
-    function f_select (s : boolean; a : integer; b: integer) return integer;
-
+    function f_select1 (g_sim : boolean; c_max1_sim : natural; c_max1_syn: natural) return natural;
+    function f_select2 (g_sim : boolean; c_max_value_sim : digit_array_t; c_max_value_syn : digit_array_t) return digit_array_t;
+    
 END stop_watch_pkg;
 
 package body stop_watch_pkg is
 
-    function f_select (s : boolean; a : integer; b: integer) return integer is
+    function f_select1 (g_sim : boolean; c_max1_sim : natural; c_max1_syn: natural) return natural is
     begin
-        if s then
-            return a;
+        if g_sim then
+            return c_max1_sim;
         else
-            return b;
+            return c_max1_syn;
+        end if;
+    end function;
+    
+    function f_select2 (g_sim : boolean; c_max_value_sim : digit_array_t; c_max_value_syn : digit_array_t) return digit_array_t is
+    begin
+        if g_sim then
+            return c_max_value_sim;
+        else
+            return c_max_value_syn;
         end if;
     end function;
     
